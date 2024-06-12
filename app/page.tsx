69e90df9d51a03023322ae943ad33bce0e977e85
Alex <87208475+K0uzer@users.noreply.github.com>
2024-06-12 21:59:10 +0300

    feat: Add content on page from books array

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,41 @@
-// Import Image from 'next/image'
+'use client'
+
+import getBooks from '@/API/getBooks'
+import { Book } from '@/types/booksTypes'
+import Image from 'next/image'
+
+import { useEffect, useState } from 'react'
 
 export default function Home() {
-    return <main className="">12</main>
+    const [books, setBook] = useState<Book[]>([])
+    useEffect(() => {
+        const fetch = async () => {
+            const booksData: Book[] = await getBooks()
+            setBook(booksData)
+        }
+        fetch()
+    }, [])
+    return (
+        <main className="">
+            <ul>
+                {books.map((book: Book) => (
+                    <li key={book.title}>
+                        <span>{book.title}</span>
+                        <span>{book.authors}</span>
+                        <span>{book.publishedDate}</span>
+                        <Image
+                            src={book.image}
+                            alt={book.title}
+                            width={100}
+                            height={100}
+                        />
+                        {/* <span>{book.language}</span> */}
+                        {/* <span>{book.price}</span> */}
+                        {/* <span>{book.currency}</span> */}
+                        {/* <span>{book.categories}</span> */}
+                    </li>
+                ))}
+            </ul>
+        </main>
+    )
 }
