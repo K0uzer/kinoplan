d3b8be9184cd43a2874d5e7121f8e0a47a1a698f
Alex <87208475+K0uzer@users.noreply.github.com>
2024-08-03 23:49:51 +0300

    feat: Add change path

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,35 +1,44 @@
 'use client'
 import React from 'react'
 import Link from 'next/link'
+import { message } from 'antd'
 
 import CartList from '@app/components/cart/CartList'
 import { useBook } from '@hooks/useBook'
 
-import styles from './cart.module.css'
+import styles from './page.module.css'
 
 const CartPage = () => {
     const { setCart } = useBook()
 
-    const payForThePurchase = () => {
+    const [messageApi, contextHolder] = message.useMessage()
+
+    const changePath = () => (window.location.href = '/')
+
+    const success = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault()
+        messageApi.open({
+            type: 'success',
+            content: 'Поздравляю с покупкой!',
+        })
         setCart([])
-        alert('Оплачено')
+
+        setTimeout(() => {
+            changePath()
+        }, 3000)
     }
 
     return (
         <div className={styles.cartWrapper}>
             <CartList />
             <div className={styles.buttonWrapper}>
+                {contextHolder}
                 <Link href="/">
                     <button className={styles.button}>Вернуться назад</button>
                 </Link>
-                <Link href="/">
-                    <button
-                        onClick={payForThePurchase}
-                        className={styles.button}
-                    >
-                        Оплатить
-                    </button>
-                </Link>
+                <button onClick={success} className={styles.button}>
+                    Оплатить
+                </button>
             </div>
         </div>
     )
