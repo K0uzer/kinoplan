4fe47cf801d9acdd195c4fa92c9e09ee46340d94
Alex <87208475+K0uzer@users.noreply.github.com>
2024-09-24 23:03:46 +0300

    refactor: cookies and URL changes

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,11 +1,3 @@
 export const reloadPage = () => window.location.reload()
 
 export const changePage = (path: string) => (window.location.href = path)
-
-export const getCookie = () => {
-    return document.cookie.split('; ').reduce((acc, item) => {
-        const [name, value] = item.split('=')
-        acc[name] = value
-        return acc
-    }, {})
-}
--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -16,7 +16,7 @@ export async function updateSession(request: NextRequest) {
                     return request.cookies.getAll()
                 },
                 setAll(cookiesToSet) {
-                    cookiesToSet.forEach(({ name, value, options }) =>
+                    cookiesToSet.forEach(({ name, value }) =>
                         request.cookies.set(name, value),
                     )
                     supabaseResponse = NextResponse.next({
@@ -36,11 +36,12 @@ export async function updateSession(request: NextRequest) {
 
     if (
         !user &&
+        !request.nextUrl.pathname.startsWith(PATH.AUTH) &&
         !request.nextUrl.pathname.startsWith(PATH.PROFILE) &&
         !request.nextUrl.pathname.startsWith(PATH.MAIN)
     ) {
         const url = request.nextUrl.clone()
-        url.pathname = PATH.PROFILE
+        url.pathname = PATH.AUTH
         return NextResponse.redirect(url)
     }
 
